743045f14a93cd3b90b789a3b7c7c10704b93787
Joezeo <1252795700@qq.com>
2017-12-11 22:55:43 +0800

    增加循环👀

--- data_main.py
+++ data_main.py
@@ -8,19 +8,23 @@ class DataMain():
         self.dataplot = data_plot.DataPlot()
 
     def start(self):
-        print('1.查看时间与用户总数的关系')
-        print('2.查看时间与注册用户的关系')
-        print('3.查看时间与未注册用户的关系')
-        cho = int(input('请输入你的选择 -->'))
         data_dteday = data_cnt = data_casual = data_registered = []
         data_dteday, data_cnt, data_casual, data_registered =\
         self.database.pandas_connect()
-        if cho == 1:
-            self.dataplot.plot_data(data_dteday, data_cnt)
-        if cho == 2:
-            self.dataplot.plot_data(data_dteday, data_registered)
-        if cho == 3:
-            self.dataplot.plot_data(data_dteday, data_casual)
+        while True:
+            print('\n\n0.退出程序')
+            print('1.查看时间与用户总数的关系')
+            print('2.查看时间与注册用户的关系')
+            print('3.查看时间与未注册用户的关系')
+            cho = int(input('请输入你的选择 -->'))
+            if cho == 0:
+                break    
+            if cho == 1:
+                self.dataplot.plot_data(data_dteday, data_cnt)
+            if cho == 2:
+                self.dataplot.plot_data(data_dteday, data_registered)
+            if cho == 3:
+                self.dataplot.plot_data(data_dteday, data_casual)
 
 
 if __name__ == "__main__":
